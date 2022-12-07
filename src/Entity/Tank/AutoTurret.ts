30d52101690d1a2fdce7e51e7add2358e9b4ba02
fractallized <101606568+fractallized@users.noreply.github.com>
2022-12-07 01:13:53 +0000

    added a test turret

--- src/Entity/Tank/AutoTurret.ts
+++ src/Entity/Tank/AutoTurret.ts
@@ -107,6 +107,10 @@ export default class AutoTurret extends ObjectEntity {
 
         this.turret = [new Barrel(this, turretDefinition)];
         this.turret[0].physicsData.values.flags |= PhysicsFlags._unknown;
+        const def = {...turretDefinition};
+        def.angle += 1;
+        this.turret.push(new Barrel(this,def));
+        this.turret[1].physicsData.values.flags |= PhysicsFlags._unknown;
     }
     
     /**
