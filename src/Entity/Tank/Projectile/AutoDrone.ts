d94066eb6610b66297c4f1e96f39f54f4b91627b
fractallized <101606568+fractallized@users.noreply.github.com>
2022-12-07 20:33:03 +0000

    old autodrone

--- src/Entity/Tank/Projectile/AutoDrone.ts
+++ src/Entity/Tank/Projectile/AutoDrone.ts
@@ -18,7 +18,6 @@
 
 import Barrel from "../Barrel";
 import Bullet from "./Bullet";
-import Drone from "./Drone";
 import { Inputs } from "../../AI";
 import { PhysicsFlags, StyleFlags } from "../../../Const/Enums";
 import { TankDefinition } from "../../../Const/TankDefinitions";
@@ -30,7 +29,7 @@ import AutoTurret from "../AutoTurret";
 /**
  * The drone class represents the drone (projectile) entity in diep.
  */
-export default class AutoDrone extends Drone implements BarrelBase {
+export default class AutoDrone extends Bullet implements BarrelBase {
     private turret: AutoTurret;
     public sizeFactor: number;
     public cameraEntity: Entity;
@@ -43,7 +42,7 @@ export default class AutoDrone extends Drone implements BarrelBase {
     public static MAX_RESTING_RADIUS = 400 ** 2;
 
     /** Used let the drone go back to the player in time. */
-    protected restCycle = true;
+    private restCycle = true;
 
 
     /** Cached prop of the definition. */
@@ -77,6 +76,7 @@ export default class AutoDrone extends Drone implements BarrelBase {
         this.sizeFactor = this.physicsData.values.size / 50;
         this.turret = new AutoTurret(this, autoCannonDef);
         this.turret.positionData.values.angle = shootAngle
+        //atuo.ai.passiveRotation = this.movementAngle
         this.turret.styleData.values.flags |= StyleFlags.showsAboveParent;
         this.turret.ai.viewRange = 640
         const bulletDefinition = barrel.definition.bullet;
@@ -104,6 +104,7 @@ export default class AutoDrone extends Drone implements BarrelBase {
         this.physicsData.values.absorbtionFactor = bulletDefinition.absorbtionFactor;
 
         this.baseSpeed /= 3;
+
         barrel.droneCount += 1;
 
         this.ai.movementSpeed = this.ai.aimSpeed = this.baseAccel;
@@ -115,8 +116,50 @@ export default class AutoDrone extends Drone implements BarrelBase {
 
         super.destroy(animate);
     }
+    
 
     public tick(tick: number) {
-        super.tick(tick);
+        this.reloadTime = this.tank.reloadTime;
+        const usingAI = !this.canControlDrones || this.tank.inputs.deleted || (!this.tank.inputs.attemptingShot() && !this.tank.inputs.attemptingRepel());
+        const inputs = !usingAI ? this.tank.inputs : this.ai.inputs;
+        if (usingAI && this.ai.state === AIState.idle) {
+            const delta = {
+                x: this.positionData.values.x - this.tank.positionData.values.x,
+                y: this.positionData.values.y - this.tank.positionData.values.y
+            }
+            const base = this.baseAccel;
+
+            // still a bit inaccurate, works though
+            let unitDist = (delta.x ** 2 + delta.y ** 2) / AutoDrone.MAX_RESTING_RADIUS;
+            if (unitDist <= 1 && this.restCycle) {
+                this.baseAccel /= 6;
+                this.positionData.angle += 0.01 + 0.012 * unitDist;
+            } else {
+                const offset = Math.atan2(delta.y, delta.x) + Math.PI / 2
+                delta.x = this.tank.positionData.values.x + Math.cos(offset) * this.tank.physicsData.values.size * 1.2 - this.positionData.values.x;
+                delta.y = this.tank.positionData.values.y + Math.sin(offset) * this.tank.physicsData.values.size * 1.2 - this.positionData.values.y;
+                this.positionData.angle = Math.atan2(delta.y, delta.x);
+                if (unitDist < 0.5) this.baseAccel /= 3;
+                this.restCycle = (delta.x ** 2 + delta.y ** 2) <= 4 * (this.tank.physicsData.values.size ** 2);
+            }
+
+            if (!Entity.exists(this.barrelEntity)) this.destroy();
+
+            this.baseAccel = base;
+
+            return;
+        } else {
+            this.positionData.angle = Math.atan2(inputs.mouse.y - this.positionData.values.y, inputs.mouse.x - this.positionData.values.x);
+            this.restCycle = false
+        }
+
+
+        
+        if (this.canControlDrones && inputs.attemptingRepel()) this.positionData.angle += Math.PI; 
+
+        // So that switch tank works, as well as on death
+        if (!Entity.exists(this.barrelEntity)) this.destroy();
+
+        this.tickMixin(tick);
     }
 }
