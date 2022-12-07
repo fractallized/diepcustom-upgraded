f552de960e39fe7d7a2451eddc77be52dda83b46
fractallized <101606568+fractallized@users.noreply.github.com>
2022-12-07 01:11:09 +0000

    fixed breaking bug

--- src/Entity/Tank/Addons.ts
+++ src/Entity/Tank/Addons.ts
@@ -424,7 +424,7 @@ class AutoRocketAddon extends Addon {
 
         new LauncherAddon(base);
 
-        base.turret.styleData.zIndex += 2;
+        for (const t of base.turret) t.styleData.zIndex += 2;
     }
 }
 /** SPIESK addon. */
